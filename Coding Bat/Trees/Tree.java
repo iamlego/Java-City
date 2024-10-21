5fd1e266e411541dc94ccf37927f0e1d97c049bb
iamlego <sairaj.jadhav812@gmail.com>
2024-10-20 23:50:40 -0700

    Trees

---
+++ Coding Bat/Trees/Tree.java
@@ -0,0 +1,27 @@
+
+class Tree {
+    public static int findClosestValueInBst(BST tree, int target) {
+      // Write your code here.
+      
+      return -1;
+    }
+  
+    static class BST {
+      public int value;
+      public BST left;
+      public BST right;
+  
+      public BST(int value) {
+        this.value = value;
+      }
+    }
+
+    public static void main(String[] args) {
+        BST b1 = new BST(10);
+
+        System.err.println(b1);
+
+    }
+
+  }
+  
